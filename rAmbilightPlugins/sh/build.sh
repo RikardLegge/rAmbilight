42180579a095be0563d44ed24fdd0458d12c1a85
Rikard Legge <rikard.legge@gmail.com>
2014-11-22 19:16:25 +0100

    removed built class files from commit

--- rAmbilightPlugins/sh/build.sh
+++ rAmbilightPlugins/sh/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-JAR="/Users/me/Library/Application Support/rAmbilight/plugins"
-CLASSROOT="../build/classes/"
+PLUGINS="~/Library/Application Support/rAmbilight/plugins"
+CLASSROOT="../bin/classes/"
 CLASS="com/rambilight/plugins"
-if [ ! -d "$JAR" ]; then
-  mkdir "$JAR"
-  ls
+if [ ! -d "$PLUGINS" ]; then
+  mkdir "$PLUGINS"
 fi
 
 for part in $(echo $1 | tr "." "\n"); do
     NAME="$part"
-    jar cvf "${JAR}/${NAME}.jar" -C "${CLASSROOT}" "${CLASS}/${NAME}"
-break;done
+    jar cvf "${PLUGINS}/${NAME}.jar" -C "${CLASSROOT}" "${CLASS}/${NAME}"
+    break;
+done
