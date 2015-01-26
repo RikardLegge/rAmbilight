5181663a6f2a7c8b78598d3a9626ea43854ea4c3
Rikard Legge <rikard.legge@gmail.com>
2015-01-26 20:26:34 +0100

    Started organising the Strings.

---
+++ rAmbilight/src/com/rambilight/core/preferences/i18n.java
@@ -0,0 +1,26 @@
+package com.rambilight.core.preferences;
+
+import java.util.Hashtable;
+
+public class i18n {
+
+    private static Hashtable<String, String> translations = new Hashtable<>();
+
+    public static String get(String identifier) {
+        if (translations.containsKey(identifier))
+            return translations.get(identifier);
+        else {
+            System.out.println(String.format("No value registered for identifier: %s", identifier));
+            return identifier;
+        }
+    }
+
+    public static void load() {
+        translations.put("noSerialPortSpecified", "No serial port specified. Finding most appropriate port...");
+        translations.put("onePortFound", "One port found: %s");
+        translations.put("portFound", "Port found!");
+        translations.put("usePort?", "Are you sure you want to use this port as an rAmbilight device? (Type 'Yes' or 'No')'\n%s\n");
+        translations.put("key", "value");
+    }
+
+}
