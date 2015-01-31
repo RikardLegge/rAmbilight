ca2faedd3f88a9464875430abb36d2bb571547a3
Rikard Legge <rikard.legge@gmail.com>
2015-01-31 21:44:11 +0100

    Added new strings

--- rAmbilight/src/com/rambilight/core/preferences/i18n.java
+++ rAmbilight/src/com/rambilight/core/preferences/i18n.java
@@ -1,34 +1,34 @@
 package com.rambilight.core.preferences;
 
-import java.util.Hashtable;
-
 /**
  * Beelining of an implementation of a version of i18n, Localization.
  * Currently only for cleaning the code from strings.
  */
 public class i18n {
 
-    private static Hashtable<String, String> translations = new Hashtable<>();
-
     public static String noSerialPortSpecified = "No serial port specified. Finding most appropriate port...";
     public static String onePortFound          = "One port found: %s";
     public static String portFound             = "Port found!";
-    public static String usePortQuestion       = "Are you sure you want to use this port as an rAmbilight device? (Type 'Yes' or 'No')'\n" +
-            "%s\n";
+    public static String usePortQuestion       = "Are you sure you want to use this port as an rAmbilight device? (Type 'Yes' or 'No')\n'%s'\n";
     public static String activating            = "Activating...";
     public static String portSelect            = "Port select";
     public static String selectPortActivation  = "%sWhich port should be activated? (Type the line number in the box below. 'E' to exit.)\n%s";
     public static String noPortSelectShutDown  = "No port was chosen. Shutting down...";
 
-    public static String notValidInput      = "%s isn't a valid input!\n";
-    public static String connectingToDevice = "Connecting to device";
-    public static String portBusy           = "The port seems to be used by another application";
-    public static String portBusyLong       = "The port seems to be used by another application, please close any other application which might communicate with the device.\nIf you don't know of any application which might be running and be connected to the USB device, try removing and reinserting the USB cable it into the computer.";
+    public static String notValidInput = "%s isn't a valid input!\n";
+    public static String portBusy      = "The port seems to be used by another application";
+    public static String portBusyLong  = "The port seems to be used by another application, please close any other application which might communicate with the device.\nIf you don't know of any application which might be running and be connected to the USB device, try removing and reinserting the USB cable it into the computer.";
 
-    public static String baudRate       = "Serial baud rate: %d";
-    public static String reinsertCable  = "Please reinsert the USB cable";
-    public static String hasHalted      = "The system seems to have halted.";
-    public static String connectionLost = "Lost connection to the USB device.";
-    public static String key            = "val";
+    public static String baudRate             = "Serial baud rate: %d";
+    public static String reinsertCable        = "Please reinsert the USB cable";
+    public static String reinsertCableConfirm = "Confirm cable reinsertion";
+    public static String hasHalted            = "The system seems to have halted";
+    public static String connectionLost       = "Lost connection to the USB device";
+    public static String suspending           = "Suspending...";
+    public static String resuming             = "Resuming...";
+    public static String connectingToDevice   = "Connecting to device";
+    public static String connectionFailed     = "Failed to connect!";
+    public static String noDeviceConnected    = "No device connected";
+    public static String key                  = "val";
 
 }
